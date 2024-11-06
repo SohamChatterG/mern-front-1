ae94097625632a616919656875e74b21437f0e6a
Soham Chatterjee <heyiamsoham@gmail.com>
2024-11-07 02:02:55 +0530

    vite config js edited

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000,  // Choose a port that Render supports, like 10000
+    host: '0.0.0.0',  // Bind to all interfaces to make it accessible externally
+  },
 })
